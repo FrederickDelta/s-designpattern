c301f6c3f1e49ab90adad05a776b841dc268e5a6
Frederick D <FrederickDelta@gmail.com>
2020-11-27 16:23:32 +0800

    📦 NEW: Mediator pattern - before

---
+++ src/com/frederick/mediator/Button.java
@@ -0,0 +1,13 @@
+package com.frederick.mediator;
+
+public class Button extends UIControl {
+    private Boolean isEnabled;
+
+    public Boolean getEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        isEnabled = enabled;
+    }
+}
---
+++ src/com/frederick/mediator/UIControl.java
@@ -0,0 +1,4 @@
+package com.frederick.mediator;
+
+public class UIControl {
+}
