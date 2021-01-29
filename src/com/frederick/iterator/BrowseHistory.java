f391d6583a8b8b94b83275338c80f91300af7be4
Frederick D <FrederickDelta@gmail.com>
2021-01-30 01:28:11 +0800

    👌 IMPROVE: Iterator pattern

--- src/com/frederick/iterator/BrowseHistory.java
+++ src/com/frederick/iterator/BrowseHistory.java
@@ -4,32 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BrowseHistory {
-    private List<String> urls = new ArrayList<String>();
-    // private String[] urls = new String[10];
+    private List<String> urls = new ArrayList<>();
+//    private String[] urls1 = new String[10];
+//    private CollectionType<String> urls = new CollectionType<>();
 
     public void push(String url){
         urls.add(url);
     }
 
-    public String pop(){
-        var lastIndex = urls.size() -1;
-        var lastItem = urls.get(lastIndex);
-        urls.remove(lastIndex);
-
-        return lastItem;
-    }
+//    public String pop(){
+//        var lastIndex = urls.size() -1;
+//        var lastItem = urls.get(lastIndex);
+//        urls.remove(lastIndex);
+//
+//        return lastItem;
+//    }
 
     public Iterator createIterator(){
         return new ListIterator(this);
     }
 
-    public class ListIterator implements Iterator {
+    public class ListIterator implements Iterator<String> {
         private BrowseHistory history;
         private int index;
 
         public ListIterator(BrowseHistory history){
             this.history = history;
         }
+
         @Override
         public String current() {
             return history.urls.get(index);
@@ -45,6 +47,4 @@ public class BrowseHistory {
             index++;
         }
     }
-
-    // public class ArrayIterator implements Iterator {}
 }
--- src/com/frederick/iterator/Iterator.java
+++ src/com/frederick/iterator/Iterator.java
@@ -1,7 +1,7 @@
 package com.frederick.iterator;
 
-public interface Iterator {
-    String current();
+public interface Iterator<T> {
+    T current();
     boolean hasNext();
     void next();
 }
